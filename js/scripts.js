d813edb4d40edcad86afcdcc88d5b505807b5051
EliaPennoni <elia.pennoni1991@gmail.com>
2024-07-28 18:06:04 +0200

    esercizio terminato

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,65 @@
 console.log('js connected');
+
+const ourTeam = [
+    {
+        name: 'Wayne Barnett',
+        role: 'Founder & CEO',
+        image: 'wayne-barnett-founder-ceo.jpg'
+    },
+    {
+        name: 'Angela Caroll',
+        role: 'Chief Editor',
+        image: 'angela-caroll-chief-editor.jpg'
+    },
+    {
+        name: 'Walter Gordon',
+        role: 'Office Manager',
+        image: 'walter-gordon-office-manager.jpg'
+    },
+    {
+        name: 'Angela Lopez',
+        role: 'Social Media Manager',
+        image: 'angela-lopez-social-media-manager.jpg'
+    },
+    {
+        name: 'Scott Estrada',
+        role: 'Developer',
+        image: 'scott-estrada-developer.jpg'
+    },
+    {
+        name: 'Barbara Ramos',
+        role: 'Graphic Designer',
+        image: 'barbara-ramos-graphic-designer.jpg'
+    }
+];
+
+console.log('ourTeam', ourTeam, typeof ourTeam);
+console.log('ourTeam[0]', ourTeam[0], typeof ourTeam[0]);
+console.log('ourTeam[1]', ourTeam[1], typeof ourTeam[1]);
+console.log('ourTeam[2]', ourTeam[2], typeof ourTeam[2]);
+console.log('ourTeam[3]', ourTeam[3], typeof ourTeam[3]);
+console.log('ourTeam[4]', ourTeam[4], typeof ourTeam[4]);
+console.log('ourTeam[5]', ourTeam[5], typeof ourTeam[5]);
+
+const teamContainer = document.getElementById('team-container');
+
+for (let i = 0; i < ourTeam.length; i++) {
+    console.log(ourTeam[i]);
+
+    teamContainer.innerHTML += `
+        <ul>
+            <li>
+                Name: <strong>${ourTeam[i].name}</strong>
+            </li>
+            <li>
+                Role: <strong>${ourTeam[i].role}</strong>
+            </li>
+            <li>
+                Image:
+                <div>
+                    <img src="img/${ourTeam[i].image}" alt="${ourTeam[i].name}">
+                </div>
+            </li>
+        </ul>
+    `;
+}
